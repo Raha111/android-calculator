65ffe8f7ff501313eb9efac44d7b864d159f83d7
Raha111 <rubaiyaraktinraha@gmail.com>
2023-03-11 19:44:12 +0600

    A new java class added

---
+++ app/src/main/java/zone/eloy/projects/androidcalculator/rafi.java
@@ -0,0 +1,18 @@
+package zone.eloy.projects.androidcalculator;
+
+public class rafi {
+
+    public String name;
+
+    public rafi(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
